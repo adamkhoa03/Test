e1eac0f435a092d478c554d73b3157f5b3a62d62
Minh Khoa <adamkhoa03@gmail.com>
2022-03-03 22:41:16 +0700

    cm23

--- hello.php
+++ hello.php
@@ -1,7 +1,24 @@
 <?php
 echo 1;
 echo 2;
-echo 3;
-echo 1;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
+echo 2;
+echo 3;echo 1;
 echo 2;
 echo 3;
